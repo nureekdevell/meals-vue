433d1e855d7f6249f71d899abdf6f74d5055f9c2
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-01-26 02:47:31 +0500

    added letters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <input type="text" class="rounded border-gray-400 w-full">
+  <div class="flex flex-col p-8 ">
+    <input type="text" class="rounded border-gray-400 w-full" placeholder="Search for Meals">
+
+    <div class="flex gap-1">
+      <router-link to="" v-for="letter of letters.split('')">{{letter}}</router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,6 +13,7 @@ import {computed} from "vue";
 import store from "../store/index.js";
 
 const meals = computed(() => store.state.meals)
+const letters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 </script>
 
 <style lang="scss" scoped>
